24f8a4b511d62e1c87d777f90a11f76b0fef3946
Ivan Klarman <klarman.ivan@gmail.com>
2014-06-14 16:27:31 +0200

    Correction problème sur deckbuilder
    correction passage souris sur le bouton DeckBuilder

--- deckBuilder.py
+++ deckBuilder.py
@@ -23,30 +23,71 @@ def deckBuilder_main(fenetre):
     fenetre.blit(button_modify, (700,500))
     fenetre.blit(button_delete, (700,550))
     fenetre.blit(button_back, (700,600))
-
-    #Initialisation de la liste des Decks
-    listDeck = {}
-    cpt = 0
-    # Style des noms de decks
-    red = (255, 255, 255)
-    myfont = pygame.font.SysFont("Arial", bold=true,30)
-    # Liste des fichiers dans le dossier decklist
-    listfile = os.listdir("decks/decklist/")
-    for file in listfile:
-        listDeck = {"id": cpt,"name": os.path.splitext(file)[0] }
-        cpt += 1
-        label = myfont.render(listDeck["name"], 1, red)
-        fenetre.blit(label, (400,300+(cpt*45)+10))
-        print(listDeck)
-        #print(os.path.splitext(file)[0])
-    #Recuperation des donnees Json des fichier
-    with open("decks/decklist/deck_base.json") as json_file:
-        json_data = json.load(json_file)
-        #Affiche le tableau de valeurs
-        #print(json_data)
-    continuer = 1
+    back_r = button_back.get_rect()
+    back_r.x, back_r.y = 700,600
+    
+    create_r = button_create.get_rect()
+    create_r.x, create_r.y = 700,450
+    
+    modify_r = button_modify.get_rect()
+    modify_r.x, modify_r.y = 700,500
+    
+    delete_r = button_delete.get_rect()
+    delete_r.x, delete_r.y = 700,550
+    
     pygame.display.flip()
+    continuer = 1
     while continuer:
-        continuer +=1
-        if continuer > 500:
-            continuer = 0
+        deckBuilder_routine(fenetre)
+        for event in pygame.event.get():
+            if event.type == pygame.quit:
+                continuer = 0
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                #Bouton BACK
+                if ( x in range(700, 825)) and (y in range(600,625)):
+                    continuer = 0
+                if ( x in range(700, 675)) and (y in range(450,625)):
+                    continuer = 0
+                if ( x in range(700, 725)) and (y in range(500,625)):
+                    continuer = 0
+                if ( x in range(700, 775)) and (y in range(550,625)):
+                    continuer = 0
+        #Initialisation de la liste des Decks
+        listDeck = {}
+        cpt = 0
+        # Style des noms de decks
+        red = (255, 255, 255)
+        myfont = pygame.font.SysFont("Arial",30)
+        # Liste des fichiers dans le dossier decklist
+        listfile = os.listdir("decks/decklist/")
+        for file in listfile:
+            listDeck = {"id": cpt,"name": os.path.splitext(file)[0] }
+            cpt += 1
+            label = myfont.render(listDeck["name"], 1, red)
+            fenetre.blit(label, (40,30+(cpt*45)+10))
+            # Si l'on passe la souris sur le label alors ...
+            if listDeck.get_rect().collipoint(pygame.mouse.get_pos()):
+                
+            print(listDeck)
+            pygame.display.flip()
+            #print(os.path.splitext(file)[0])
+        #Recuperation des donnees Json des fichier
+        with open("decks/decklist/deck_base.json") as json_file:
+            json_data = json.load(json_file)
+            pygame.display.flip()
+            #Affiche le tableau de valeurs
+            #print(json_data)
+def deckBuilder_routine(fenetre):
+    pygame.display.set_caption("Galactica - Deck Builder")
+    fond_deckBuilder = pygame.image.load("images/background.jpg").convert()
+    button_modify = pygame.image.load("images/modify_no_highlight.jpg").convert()
+    button_modify_h = pygame.image.load("images/modify_highlight.jpg").convert()
+    button_delete = pygame.image.load("images/delete_no_highlight.jpg").convert()
+    button_delete_h = pygame.image.load("images/delete_highlight.jpg").convert()
+    button_create = pygame.image.load("images/create_no_highlight.jpg").convert()
+    button_create_h = pygame.image.load("images/create_highlight.jpg").convert()
+    button_back = pygame.image.load("images/back_no_highlight.jpg").convert()
+    button_back_h = pygame.image.load("images/back_highlight.jpg").convert()
+    title = pygame.image.load("images/title.png").convert()
+    pygame.display.flip()
